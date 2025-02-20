b42b9723dd198d8d1efba89b7bce37a35d2b3c54
Lxi0707 <888qopo@gmail.com>
2025-02-21 03:08:35 +0800

    Update XiJiu.js

--- Task/XiJiu/XiJiu.js
+++ Task/XiJiu/XiJiu.js
@@ -8,7 +8,7 @@ const $ = new Env('习酒');
 const notify = $.isNode() ? require('../sendNotify') : '';
 const XiJiu = ($.isNode() ? JSON.parse(process.env.XiJiu) : $.getjson("XiJiu")) || [];
 const XiJiu_Exchange = ($.isNode() ? process.env.XiJiu_Exchange : $.getdata("XiJiu_Exchange")) === 'true' || false;
-const OCR_SERVER = ($.isNode() ? process.env.OCR_SERVER : $.getdata("OCR_SERVER")) || 'http://ocr.1pro.top';
+const OCR_SERVER = ($.isNode() ? process.env.OCR_SERVER : $.getdata("OCR_SERVER")) || 'http://ocr.1pro.top:80';
 let cropType = [{"1":"高粱"},{"2":"小麦"}];
 let loginCode = '';
 let token = '';
